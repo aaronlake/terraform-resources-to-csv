c3b6be2601f0ae49031f46e2e43efba8d06f9c78
Aaron Lake <aaron.lake@gmail.com>
2022-11-04 20:33:46 -0400

    Add next page support, closes issue #1

--- tfe_resources.py
+++ tfe_resources.py
@@ -5,26 +5,12 @@
 # Supply URL if not using https://app.terraform.io
 # ./tfe_resources.py --url https://terraform.companyname.com --output file.csv
 
-import requests
-import argparse
-import os
 import csv
+import os
+import argparse
+import requests
 
-parser = argparse.ArgumentParser(description='Parse arguments')
-parser.add_argument('--url', help='Parse TFE URL, leave blank for TFC',
-                    default='https://app.terraform.io', required=False)
-parser.add_argument('--output', help='Output file name', required=True)
-
-args = parser.parse_args()
-
-# Get TFE_TOKEN environment variable
-token = os.environ.get('TFE_TOKEN')
-headers = {'Authorization': 'Bearer ' + token,
-           'Content-Type': 'application/vnd.api+json'}
-
-if token is None:
-    print('Environment variable TFE_TOKEN not set')
-    exit(1)
+description = 'Terraform Cloud/Enterprise Resource Inventory to CSV'
 
 
 class organizations:
@@ -54,17 +40,23 @@ class resources:
 
 def get_organizations():
     list = []
-    url = args.url + '/api/v2/organizations'
-    r = requests.get(url, headers=headers)
+    next_page = args.url + '/api/v2/organizations'
+    while next_page:
+        page = requests.get(next_page, headers=HEADERS)
 
-    if r.status_code == 200:
-        for org in r.json()['data']:
-            list.append(organizations(
-                org['id'], org['attributes']['created-at']))
+        if page.status_code == 200:
+            for org in page.json()['data']:
+                list.append(organizations(
+                    org['id'], org['attributes']['created-at']))
 
-    else:
-        print('Error ({}) getting organizations'.format(r.status_code))
-        exit(1)
+        else:
+            print('Error ({}) getting organizations'.format(page.status_code))
+            exit(1)
+
+        try:
+            next_page = page.json()['links']['next']
+        except KeyError:
+            next_page = None
 
     return list
 
@@ -77,19 +69,27 @@ def get_workspaces(orgs):
     list = []
 
     for org in orgs:
-        url = args.url + '/api/v2/organizations/{}/workspaces'.format(org.id)
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
-            for workspace in r.json()['data']:
-                list.append(workspaces(workspace['id'],
-                                       workspace['attributes']['name'],
-                                       org.id,
-                                       workspace['attributes']['created-at'],
-                                       workspace['attributes']['updated-at']))
-        else:
-            print('Error ({}) getting workspaces for "{}" organization'.format(
-                r.status_code, org.id))
-            exit(1)
+        next_page = args.url + \
+            '/api/v2/organizations/{}/workspaces'.format(org.id)
+        while next_page:
+            page = requests.get(next_page, headers=HEADERS)
+            if page.status_code == 200:
+                for workspace in page.json()['data']:
+                    list.append(workspaces(workspace['id'],
+                                           workspace['attributes']['name'],
+                                           org.id,
+                                           workspace['attributes']['created-at'],
+                                           workspace['attributes']['updated-at']))
+            else:
+                print('Error ({}) getting workspaces for "{}" organization'.format(
+                    page.status_code, org.id))
+                exit(1)
+
+            try:
+                next_page = page.json()['links']['next']
+            except KeyError:
+                next_page = None
+
     return list
 
 
@@ -103,21 +103,21 @@ def get_current_state(ws):
     list = []
 
     for workspace in ws:
-        url = args.url + \
+        next_page = args.url + \
             '/api/v2/workspaces/{}/current-state-version'.format(workspace.id)
-        r = requests.get(url, headers=headers)
+        page = requests.get(next_page, headers=HEADERS)
 
-        if (r.status_code == 200):
-            state_url = r.json()[
+        if (page.status_code == 200):
+            state_url = page.json()[
                 'data']['attributes']['hosted-state-download-url']
         else:
             print('Error ({}) getting state for "{}" workspace'.format(
-                r.status_code, workspace.name))
+                page.status_code, workspace.name))
             exit(1)
 
-        r = requests.get(state_url, headers=headers)
-        if (r.status_code == 200):
-            for resource in r.json()['resources']:
+        page = requests.get(state_url, headers=HEADERS)
+        if (page.status_code == 200):
+            for resource in page.json()['resources']:
                 name = resource['name']
                 type = resource['type']
                 for instance in resource['instances']:
@@ -132,20 +132,41 @@ def get_current_state(ws):
     return list
 
 
-orgs = get_organizations()
-ws = get_workspaces(orgs)
-res = get_current_state(ws)
+def create_csv(resources):
+    fields = ['name', 'type', 'identifier',
+              'org', 'workspace_id', 'workspace_name']
 
-fields = ['name', 'type', 'identifier',
-          'org', 'workspace_id', 'workspace_name']
+    with open(args.output, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        for resource in resources:
+            writer.writerow({'name': resource.name,
+                            'type': resource.type,
+                             'identifier': resource.identifier,
+                             'org': resource.org,
+                             'workspace_id': resource.workspace_id,
+                             'workspace_name': resource.workspace_name})
 
-with open(args.output, 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-    writer.writeheader()
-    for resource in res:
-        writer.writerow({'name': resource.name,
-                         'type': resource.type,
-                         'identifier': resource.identifier,
-                         'org': resource.org,
-                         'workspace_id': resource.workspace_id,
-                         'workspace_name': resource.workspace_name})
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--url', help='Parse TFE URL, leave blank for TFC',
+                        default='https://app.terraform.io', required=False)
+    parser.add_argument('--output', help='Output file name', required=True)
+
+    args = parser.parse_args()
+
+    # Get TFE_TOKEN environment variable
+    TOKEN = os.environ.get('TFE_TOKEN')
+    HEADERS = {'Authorization': 'Bearer ' + TOKEN,
+               'Content-Type': 'application/vnd.api+json'}
+
+    if TOKEN is None:
+        print('Environment variable TFE_TOKEN not set')
+        exit(1)
+
+    orgs = get_organizations()
+    ws = get_workspaces(orgs)
+    res = get_current_state(ws)
+    create_csv(res)
